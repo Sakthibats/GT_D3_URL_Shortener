05416f62c00b6916abe590c3623aaed7f80362ff
Sakthibats <baskaran.sakthivel@gmail.com>
2022-09-11 22:02:33 +0800

    added home navigation button

--- server.js
+++ server.js
@@ -1,38 +1,39 @@
+// Import Packages
 const express = require('express')
 const mysql = require("mysql2")
 const config = require("./config")
 request = require('request')
 
+// Setup
 const app = express()
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:false}))
 
 // Check for a heroku port
 app.set('port', (process.env.PORT || 5000));
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended:false}))
-
 // On start up render index ejs file with 3 context inputs: msg, link, error
 app.get('/', (req, res)=>{
-    res.render('index',{msg:"small Link will be here", link: '/', error:null})
+    res.render('index',{msg:"", link: '/', error:null})
 })
 
 // Takes short url from link parameter and searches database for item. If shorturl present return long url and redirect to there. Else throw an error and pass to frontend
-app.get('/app/:shorturl', (req, res)=>{
+app.get('/redirect/:shorturl', (req, res)=>{
     let shorturlid = req.params.shorturl;
 	let sql = `SELECT * FROM url WHERE ShortUrl='${shorturlid}' LIMIT 1`;
 	const conn = mysql.createConnection(config.db)
 	conn.query(sql,function(err, results, fields) {
 		if (err){
 			console.log(err)
-			res.render('index',{msg:shorturlid, link:`/app/${shorturlid}`, error: err})
+			res.render('index',{msg:shorturlid, link:`/redirect/${shorturlid}`, error: err})
 		}
 		else{
 			// Results holds all values queried. If none match shortURL=> empty array will be returned
 			if (results.length!=0){
-				console.log(`Redirected to ${results[0].LongUrl}`)
+				console.log(`Redirected to External site`)
 				res.redirect(results[0].LongUrl)
 			}else{
-				res.render('index',{msg:shorturlid, link:`/app/${shorturlid}`, error:'ShortURL not in Database'})
+				res.render('index',{msg:shorturlid, link:`/redirect/${shorturlid}`, error:'ShortURL not in Database'})
 			}
 		}
 	});
@@ -51,12 +52,11 @@ app.post('/shrinkedURLs', async(req, res)=>{
 			console.log("Pushed new row to MySQL data base")
 		}
 	});
-
     conn.end()
-    res.render('index',{msg:uniqueID, link:`/app/${uniqueID}`, error:null})
+    res.render('index',{msg:uniqueID, link:`/redirect/${uniqueID}`, error:null})
 })
 
 // Listen on assigned port
 app.listen(app.get('port'), function () {
-    console.log(`URL Shortening service listening at http://localhost:${app.get('port')}`);
+    console.log(`URL Shortening service listening at port: ${app.get('port')}`);
 });
